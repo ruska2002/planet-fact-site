9ebb0235dd1b91d396b4925200605d6ddacf4bcf
ruska metskhovrishvili <ruso.mecxovrishvili1@gmail.com>
2025-04-08 15:05:35 +0400

    Add new code feature

--- src/components/Planet.tsx
+++ src/components/Planet.tsx
@@ -2,81 +2,157 @@ import { useParams } from "react-router-dom";
 import data from "../../data.json";
 import { useState } from "react";
 
-// Define the type for the planet data structure
+const buttonName = [
+  { name: "overview", text: "OVERVIEW", mobileText: "OVERVIEW" },
+  { name: "structure", text: "Internal Structure", mobileText: "STRUCTURE" },
+  { name: "geology", text: "Surface Geology", mobileText: "SURFACE" },
+] as const;
+
 interface Planet {
   name: string;
-  overview: {
-    content: string;
-    source: string;
-  };
-  structure: {
-    content: string;
-    source: string;
-  };
-  geology: {
-    content: string;
-    source: string;
-  };
-  images: {
-    planet: string;
-    internal: string;
-    geology: string;
-  };
+  overview: { content: string; source: string };
+  structure: { content: string; source: string };
+  geology: { content: string; source: string };
+  images: { planet: string; internal: string; geology: string };
   color: string;
-  image: string;
+  rotation: string;
+  revolution: string;
+  radius: string;
+  temperature: string;
+  sizes: {
+    mobileWidth: string;
+    mobileHeight: string;
+    tabletWidth: string;
+    tabletHeight: string;
+    laptopWidth: string;
+    laptopHeight: string;
+  };
 }
 
-const imageNavigation = ["OVERVIEW", "STRUCTURE", "SURFACE"] as const;
+const viewToImageKey = {
+  overview: "planet",
+  structure: "internal",
+  geology: "geology",
+} as const;
 
 export default function Planet() {
-  const { planetName } = useParams<{ planetName: string }>(); // Type for useParams
+  const [currentText, setCurrentText] = useState<
+    "overview" | "structure" | "geology"
+  >("overview");
+  const { planetName } = useParams<{ planetName: string }>();
   const planet = data.find((planet) => planet.name === planetName) as
     | Planet
     | undefined;
 
-  // State types and initialization
-  const [currentView, setCurrentView] = useState<
-    "OVERVIEW" | "STRUCTURE" | "SURFACE"
-  >("OVERVIEW");
-  const [currentText, setCurrentText] = useState<
-    "overview" | "structure" | "geology"
-  >("overview");
+  const getDomain = (url: string) => {
+    try {
+      const parsedUrl = new URL(url);
+      let hostName = parsedUrl.hostname;
 
-  if (!planet) {
-    return <p>Planet not found</p>;
-  }
+      //hostname aris properti romelic abrunebs aqedan https://en.wikipedia.org/wiki/Mercury_(planet) amas en.wikipedia.org
+
+      // ვშლი www , en, ან მსგვას ტექსტს დასაწყისიდან
+      hostName = hostName.replace(/^(www\.|en\.)/, "");
+      // აქ ვშლი ბოლოდან .ორგ-ს
+      hostName = hostName.replace(/\.org$/, "");
+
+      return hostName;
+    } catch (error) {
+      console.error("Invalid URL", error);
+      return "Invalid URL";
+    }
+  };
+
+  const domain = getDomain(planet?.[currentText]?.source);
+
+  const getImageSize = (view: "planet" | "internal" | "geology") => {
+    const sizes = planet?.sizes[view];
+    if (!sizes) return { width: "173px", height: "173px" };
+
+    const windowWidth = window.innerWidth;
+
+    if (windowWidth <= 640) {
+      // Mobile size
+      return { width: sizes.mobileWidth, height: sizes.mobileHeight };
+    } else if (windowWidth <= 1024) {
+      // Tablet size
+      return { width: sizes.tabletWidth, height: sizes.tabletHeight };
+    } else {
+      // Laptop size
+      return { width: sizes.laptopWidth, height: sizes.laptopHeight };
+    }
+  };
+  const { width, height } = getImageSize(viewToImageKey[currentText]);
 
   return (
     <div>
-      {/* Planet Image */}
-      <img
-        src={planet.images[currentText]}
-        alt={`${planet.name} ${currentView}`}
-      />
-
-      {/* Planet Name */}
-      <h2 className="text-[#ffffff]">{planet.name}</h2>
-
-      {/* Description */}
-      <p className="text-[#ffffff]">{planet[currentText].content}</p>
-
-      {/* View Buttons */}
-      <nav>
-        {imageNavigation.map((view, index) => (
+      <nav className="flex">
+        {buttonName.map((view, index) => (
           <button
+            className="text-[#ffffff] uppercase"
             key={index}
-            onClick={() => {
-              setCurrentView(view);
-              setCurrentText(
-                view.toLowerCase() as "overview" | "structure" | "geology"
-              ); // Dynamically change content type
-            }}
-            className={currentView === view ? "active" : ""}
+            onClick={() => setCurrentText(view.name)}
           >
-            {view}
+            <span className="block md:hidden space-between text-[10px] mt-4 mb-3 font-bold ml-6 mr-12">
+              {view.mobileText}
+            </span>
+            <span className="hidden md:block">{view.text}</span>
+            {currentText === view.name && (
+              <div
+                className="w-[75px] h-[4px] ml-4"
+                style={{ backgroundColor: planet?.color }}
+              ></div>
+            )}
           </button>
         ))}
       </nav>
+
+      <hr className=" opacity-20" />
+      <div className="text-center">
+        <img
+          src={planet?.images[viewToImageKey[currentText]]}
+          alt={planet?.name}
+          style={{ width, height }}
+          className="mt-[64px] mx-auto sm:mx-0"
+        />
+      </div>
+      <div className="ml-[21px] mr-[21px] text-center">
+        <p className="text-[#ffffff] font-normal text-[35px] uppercase mt-[50px]">
+          {planet?.name}
+        </p>
+        <p className="text-[#ffffff] text-[11px] font-medium text-center opacity-80">
+          {planet?.[currentText].content}
+        </p>
+        <p className="text-[#ffffff] text-[12px] font-normal ">
+          Source:{" "}
+          <a
+            href={planet?.[currentText].source}
+            target="_blank"
+            rel="noreferrer"
+            className="underline font-bold"
+          >
+            {domain}
+          </a>
+        </p>
+      </div>
+      <div className="text-[#ffff] uppercase text-center ml-[30px] mr-[30px] mt-8 mb-4">
+        <div className="flex justify-between mb-2 border border-white/20 items-baseline">
+          <p className="ml-[15px] text-[8px] mb-4 mt-4">ROTATION TIME</p>
+          <p className="mr-[15px]">{planet?.rotation}</p>
+        </div>
+        <div className="flex justify-between mb-2 border border-white/20 items-baseline">
+          <p className="ml-[15px] text-[8px] mb-4 mt-4">REVOLUTION TIME</p>
+          <p className="mr-[15px]">{planet?.revolution}</p>
+        </div>
+        <div className="flex justify-between mb-2 border border-white/20 items-baseline">
+          <p className="ml-[15px] text-[8px] mb-4 mt-4">radius </p>
+          <p className="mr-[15px] text-end">{planet?.radius}</p>
+        </div>
+        <div className="flex justify-between mb-2 border border-white/20 items-baseline">
+          <p className="ml-[15px] text-[8px] mb-4 mt-4">AVERAGE TEMP.</p>
+          <p className="mr-[15px]">{planet?.temperature}</p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/ResponsiveMenu.tsx
+++ src/components/ResponsiveMenu.tsx
@@ -32,29 +32,34 @@ export default function ResponsiveMenu({ isOpen }: ResponsiveMenuProps) {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
-          className="absolute top-20 left-0 w-full h-[667px] bg-[#070724]"
+          className="absolute top-12 left-0 w-full h-[585px] bg-[#070724]"
         >
-          <div className="text-xl font-semibold uppercase text-white py-10">
-            <ul className="flex flex-col">
+          <div className="text-xl font-semibold uppercase text-white py-10 ">
+            <ul className="flex flex-col space-y-4">
               {navigation.map((planet) => {
                 const planetData = data.find((p: Planet) => p.name === planet);
                 return (
-                  <li key={planet} className="flex items-center gap-4">
-                    {/* Show image if found in data.json */}
-                    {planetData?.image && (
-                      <img
-                        src={planetData.image}
-                        alt={planet}
-                        className="w-5 h-5"
-                      />
-                    )}
-                    <Link
-                      to={`/${planet}`}
-                      className="text-white text-sm font-bold"
-                    >
-                      {planet}
-                    </Link>
-                    <img src={arrow} />
+                  <li
+                    key={planet}
+                    className="flex justify-between items-center border-b border-white/10 px-6 py-4"
+                  >
+                    <div className="flex items-center gap-6">
+                      {planetData?.image && (
+                        <img
+                          src={planetData.image}
+                          alt={planet}
+                          className="w-5 h-5"
+                        />
+                      )}
+                      <Link
+                        to={`/${planet}`}
+                        className="text-white text-sm font-bold"
+                      >
+                        {planet}
+                      </Link>
+                    </div>
+
+                    <img src={arrow} className="h-4" />
                   </li>
                 );
               })}
